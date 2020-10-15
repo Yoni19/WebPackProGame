6430bddbc9663f74b657be4e8016425a4fc4d928
Yoni19 <anthony.krief@hotmail.fr>
2020-10-15 10:34:29 +0200

    Update changes

--- src/js/selection.js
+++ src/js/selection.js
@@ -2,10 +2,10 @@ const weekOrAll = () => {
     const selection = document.getElementById("selection");
     selection.innerHTML = `
       <a class="btn" href="#games/this-week">
-        <button class="btn btn_input">Les mieux noté cette semaine </button>
+        <button class="btn btn_input">Les mieux notés cette semaine </button>
       </a>
       <a class="btn" href="#games/all-time-best">
-        <button class="btn btn_input">Les mieux noté jusqu'a aujourd'hui</button>
+        <button class="btn btn_input">Les mieux notés jusqu'à aujourd'hui</button>
       </a>
     `;
   };
